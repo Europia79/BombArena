bb7f2839ae9793f53b379776ff8b16b894abe591
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-05-06 19:33:08 -0500

    Added config options + getters/setters
    Very soon, I am going to change the fields from public to private.
    
    So I added getters & setters.
    
    Also, I started to add configuration options.
    To make the plugin fully configurable.
    
    Please note that eventho I modify arenas.yml, I do NOT save it.
    BattleArena does the actual saving of the file.
    
    And I still have not figured out how to serialize and
    de-serialize the BombBlock in arenas.yml
    
    Self reminder: That future getters and setters for the
    Base Locations should go thru getExactLocation() method
    to prevent any location checks from failing.

--- src/mc/euro/demolition/BombPlugin.java
+++ src/mc/euro/demolition/BombPlugin.java
@@ -1,25 +1,28 @@
 package mc.euro.demolition;
 
 import java.util.ArrayList;
-import mc.euro.demolition.debug.DebugOn;
-import mc.euro.demolition.debug.DebugInterface;
-import mc.euro.demolition.commands.BombExecutor;
-import mc.euro.demolition.util.DetonateTimer;
-import mc.euro.demolition.util.PlantTimer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import mc.alk.arena.BattleArena;
+import mc.alk.arena.controllers.BattleArenaController;
+import mc.alk.arena.objects.spawns.SpawnInstance;
+import mc.alk.arena.serializers.SpawnSerializer;
 import mc.alk.arena.util.SerializerUtil;
 import mc.euro.demolition.appljuze.ConfigManager;
 import mc.euro.demolition.appljuze.CustomConfig;
-import mc.euro.demolition.debug.DebugOff;
+import mc.euro.demolition.commands.BombExecutor;
+import mc.euro.demolition.debug.*;
+import mc.euro.demolition.objects.BaseType;
 import mc.euro.demolition.tracker.PlayerStats;
+import mc.euro.demolition.util.DefuseCounter;
+import mc.euro.demolition.util.DetonateTimer;
+import mc.euro.demolition.util.PlantTimer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -56,8 +59,10 @@ public class BombPlugin extends JavaPlugin {
     public int PlantTime;
     public int DetonationTime;
     public int DefuseTime;
+    public int Tolerance;
     public Material BombBlock;
     public Material BaseBlock;
+    public InventoryType Baseinv;
     public String FakeName;
     public String ChangeFakeName;
     public int MaxDamage;
@@ -71,6 +76,8 @@ public class BombPlugin extends JavaPlugin {
     
     @Override  
     public void onEnable() {
+        
+        saveDefaultConfig();
 
         debug = new DebugOn(this);
         carriers = new HashMap<Integer, String>();
@@ -79,17 +86,18 @@ public class BombPlugin extends JavaPlugin {
         pTimers = new HashMap<Integer, PlantTimer>();
         dTimers = new HashMap<Integer, DetonateTimer>();
         
-        // Database Tables: bt_Demolition_*
-        ti = new PlayerStats("Demolition");
-        
         manager = new ConfigManager(this);
         arenasYml = manager.getNewConfig("arenas.yml");
-        
-        saveDefaultConfig();
+
         loadDefaultConfig();
+        
+        // Database Tables: bt_Demolition_*
+        ti = new PlayerStats(this.DatabaseTable);
         
         BattleArena.registerCompetition(this, "BombArena", "bomb", BombArena.class, new BombExecutor());
         getServer().dispatchCommand(Bukkit.getConsoleSender(), "bomb stats top " + StartupDisplay);
+        // getServer().dispatchCommand(Bukkit.getConsoleSender(), "bomb update arenas");
+        // updateArenasYml(this.BombBlock.name());
         
     }
     
@@ -98,29 +106,41 @@ public class BombPlugin extends JavaPlugin {
         // saveConfig();
     }
     
-    private void loadDefaultConfig() {
+    public void loadDefaultConfig() {
         
         getLogger().info("Loading config.yml");
-        PlantTime = getConfig().getInt("PlantTime");
-        DetonationTime = getConfig().getInt("DetonationTime");
-        DefuseTime = getConfig().getInt("DefuseTime");
+        PlantTime = getConfig().getInt("PlantTime", 8);
+        DetonationTime = getConfig().getInt("DetonationTime", 35);
+        DefuseTime = getConfig().getInt("DefuseTime", 1);
+        Tolerance = getConfig().getInt("Tolerance", 500);
+        if (Tolerance < 1) Tolerance = 1;
         BombBlock = Material.getMaterial(
-                getConfig().getString("BombBlock").toUpperCase());
+                getConfig().getString("BombBlock", "HARD_CLAY").toUpperCase());
+        // setBombSpawn(BombBlock.name());
         BaseBlock = Material.valueOf(
-                getConfig().getString("BaseBlock").toUpperCase());
-        FakeName = getConfig().getString("FakeName");
+                getConfig().getString("BaseBlock", "BREWING_STAND").toUpperCase());
+        try {
+            this.Baseinv = BaseType.convert(BaseBlock);
+        } catch (IllegalArgumentException ex) {
+            getLogger().warning("loadDefaultConfig() has thrown an IllegalArgumentException");
+            getLogger().warning("InventoryType has been set to default, BREWING");
+            this.Baseinv = InventoryType.BREWING;
+        }
+        FakeName = getConfig().getString("FakeName", "Bombs Planted Defused");
         ChangeFakeName = getConfig().getString("ChangeFakeName");
-        MaxDamage = getConfig().getInt("MaxDamage");
-        DeltaDamage = getConfig().getInt("DeltaDamage");
-        DamageRadius = getConfig().getInt("DamageRadius");
-        StartupDisplay = getConfig().getInt("StartupDisplay");
-        DatabaseTable = getConfig().getString("DatabaseTable");
+        MaxDamage = getConfig().getInt("MaxDamage", 50);
+        DeltaDamage = getConfig().getInt("DeltaDamage", 5);
+        DamageRadius = getConfig().getInt("DamageRadius", 9);
+        StartupDisplay = getConfig().getInt("StartupDisplay", 5);
+        DatabaseTable = getConfig().getString("DatabaseTable", "bombarena");
         
-        debug.log("PlantTime = " + PlantTime);
-        debug.log("DetonationTime = " + DetonationTime);
-        debug.log("DefuseTime = " + DefuseTime);
+        debug.log("PlantTime = " + PlantTime + " seconds");
+        debug.log("DetonationTime = " + DetonationTime + " seconds");
+        debug.log("DefuseTime = " + DefuseTime + " times");
+        debug.log("Tolerance = " + Tolerance + " milliseconds");
         debug.log("BombBlock = " + BombBlock.toString());
         debug.log("BaseBlock = " + BaseBlock.toString());
+        debug.log("Baseinv = " + Baseinv.toString());
         
         /* Set<String> keys = arenasYml.getConfigurationSection("arenas.bomb1.bases").getKeys(false);
         debug.log("keys.size() = " + keys.size());
@@ -132,6 +152,7 @@ public class BombPlugin extends JavaPlugin {
     }
     
     public ArrayList<Location> getBases(String a) {
+        // PATH = "arenas.{arena}.spawns.{n}.spawn"
         String path = "arenas." + a + ".bases";
         if (arenasYml.getConfigurationSection(path) != null
                 && arenasYml.getConfigurationSection(path).getKeys(false) != null
@@ -151,5 +172,172 @@ public class BombPlugin extends JavaPlugin {
         getLogger().severe("BombPlugin:getBases(String ArenaName) has failed to return a List of Locations.");
         return null;
     }
+    
+    public Location getBaseLocation(int matchID, int teamID) {
+        Location temp = this.bases.get(matchID).get(teamID);
+        return this.bases.get(matchID).get(teamID);
+    }
+    
+    public DefuseCounter getCounter(int matchID) {
+        return this.dTimers.get(matchID).getCounter();
+    }
+
+    public int getPlantTime() {
+        return PlantTime;
+    }
+
+    public void setPlantTime(int PlantTime) {
+        this.PlantTime = PlantTime;
+    }
+
+    public int getDetonationTime() {
+        return DetonationTime;
+    }
+
+    public void setDetonationTime(int DetonationTime) {
+        this.DetonationTime = DetonationTime;
+    }
+
+    public int getDefuseTime() {
+        return DefuseTime;
+    }
+
+    public void setDefuseTime(int DefuseTime) {
+        this.DefuseTime = DefuseTime;
+    }
+
+    public Material getBombBlock() {
+        return BombBlock;
+    }
+
+    public void setBombBlock(Material type) {
+        this.BombBlock = type;                      // IN MEMORY
+        this.getConfig().set("BombBlock", type.name()); // update config.yml
+        this.saveConfig();                              // save to disk (config.yml)
+        // this.updateArenasYml(this.BombBlock.name()); // update arenas.yml
+    }
+    
+    private void updateArenasYml(String x) {
+        // PATH = "arenas.arena.basesN.spawn"
+        // Map<Long, TimedSpawn> timedSpawns = BattleArena.getBAController().getArena("b2").getTimedSpawns();
+        BattleArenaController ac;
+        // ac.
+        String value = x + " 1";
+        String BombBlockString = this.BombBlock.name();
+        this.debug.log("updating arenas.yml with " + x);
+        ConfigurationSection arenas = this.arenasYml.getConfigurationSection("arenas");
+        this.debug.log("" + arenas.getKeys(false).toString());
+        for (String arena : arenas.getKeys(false)) {
+            this.debug.log("" + arena.toString());
+            ConfigurationSection spawns = this.arenasYml.getConfigurationSection("arenas." + arena);
+            this.debug.log("" + spawns.getKeys(false).toString());
+            for (String n : spawns.getKeys(false)) {
+                String path = "arenas." + arena + ".spawns." + n + ".spawn";
+                List<String> KEYS = new ArrayList(spawns.getKeys(false));
+                List<SpawnInstance> spawnables = SpawnSerializer.parseSpawnable(KEYS);
+                this.debug.log("spawnables = " + spawnables.toString());
+                for (SpawnInstance i : spawnables) {
+                    this.debug.log("string = " + i.toString());
+                    this.debug.log("Class = " + i.getClass());
+                }
+                /* String old = this.arenasYml.getItemStack(path).toString();
+                if (!old.startsWith(BombBlockString)) {
+                    this.debug.log("BombArena arenas.yml has an item/mob spawn that is NOT a Bomb! " + old);
+                    continue;
+                } */
+                getLogger().info("" + path + " has been changed from to " + value);
+                this.arenasYml.set(path, value);
+            }
+        }
+        this.arenasYml.saveConfig();
+    }
+
+    public Material getBaseBlock() {
+        return BaseBlock;
+    }
+
+    public void setBaseBlock(Material type) {
+        this.BaseBlock = type;
+        this.getConfig().set("BaseBlock", type.name());
+        this.saveConfig();
+    }
+
+    public InventoryType getBaseinv() {
+        return Baseinv;
+    }
+
+    public void setBaseinv(InventoryType type) {
+        this.Baseinv = type;
+    }
+
+    public String getFakeName() {
+        return FakeName;
+    }
+
+    public void setFakeName(String fakeName) {
+        this.FakeName = fakeName;
+        this.getConfig().set("FakeName", fakeName);
+        this.saveConfig();
+    }
+
+    public String getChangeFakeName() {
+        return ChangeFakeName;
+    }
+
+    public void setChangeFakeName(String fakeName) {
+        this.ChangeFakeName = fakeName;
+    }
+
+    public int getMaxDamage() {
+        return MaxDamage;
+    }
+
+    public void setMaxDamage(int max) {
+        this.MaxDamage = max;
+        this.getConfig().set("MaxDamage", max);
+        this.saveConfig();
+    }
+
+    public int getDeltaDamage() {
+        return DeltaDamage;
+    }
+
+    public void setDeltaDamage(int delta) {
+        this.DeltaDamage = delta;
+        this.getConfig().set("DeltaDamage", delta);
+        this.saveConfig();
+    }
+
+    public int getDamageRadius() {
+        return DamageRadius;
+    }
+
+    public void setDamageRadius(int radius) {
+        this.DamageRadius = radius;
+        this.getConfig().set("DamageRadius", radius);
+        this.saveConfig();
+    }
+
+    public int getStartupDisplay() {
+        return StartupDisplay;
+    }
+
+    public void setStartupDisplay(int num) {
+        this.StartupDisplay = num;
+        this.getConfig().set("StartupDisplay", num);
+        this.saveConfig();
+    }
+
+    public String getDatabaseTable() {
+        return DatabaseTable;
+    }
+
+    public void setDatabaseTable(String table) {
+        this.DatabaseTable = table;
+        this.getConfig().set("DatabaseTable", table);
+        this.saveConfig();
+    }
+    
+    
     
 }
