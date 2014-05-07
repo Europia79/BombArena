966be27fb5e17b05c6b960e7d6c121605425a0e5
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-05-06 20:09:55 -0500

    Handles the conversion from Material.X to InventoryType.X
    the EnumMap is now private static final.
    And it is initialized with a static method now.
    
    This is a really nice conversion class.
    
    But unfortunately, future versions will NOT have a need
    for this class.

--- src/mc/euro/demolition/objects/BaseType.java
+++ src/mc/euro/demolition/objects/BaseType.java
@@ -1,6 +1,9 @@
 package mc.euro.demolition.objects;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.EnumMap;
+import java.util.Map;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryType;
 
@@ -9,21 +12,23 @@ import org.bukkit.event.inventory.InventoryType;
  */
 public class BaseType {
     
-    public static EnumMap<Material, InventoryType> emap = new EnumMap(Material.class);
+    private static final Map<Material, InventoryType> emap = initMap();
     
-    static {
-        emap.put(Material.ANVIL, InventoryType.ANVIL);
-        emap.put(Material.BEACON, InventoryType.BEACON);
-        emap.put(Material.BREWING_STAND, InventoryType.BREWING);
-        emap.put(Material.CHEST, InventoryType.CHEST);
-        emap.put(Material.ENDER_CHEST, InventoryType.ENDER_CHEST);
-        emap.put(Material.TRAPPED_CHEST, InventoryType.CHEST);
-        emap.put(Material.ENCHANTMENT_TABLE, InventoryType.ENCHANTING);
-        emap.put(Material.DISPENSER, InventoryType.DISPENSER);
-        emap.put(Material.DROPPER, InventoryType.DROPPER);
-        emap.put(Material.FURNACE, InventoryType.FURNACE);
-        emap.put(Material.HOPPER, InventoryType.HOPPER);
-        emap.put(Material.WORKBENCH, InventoryType.WORKBENCH);
+    private static Map<Material, InventoryType> initMap() {
+        Map<Material, InventoryType> temp = new EnumMap<Material, InventoryType>(Material.class);
+        temp.put(Material.ANVIL, InventoryType.ANVIL);
+        temp.put(Material.BEACON, InventoryType.BEACON);
+        temp.put(Material.BREWING_STAND, InventoryType.BREWING);
+        temp.put(Material.CHEST, InventoryType.CHEST);
+        temp.put(Material.ENDER_CHEST, InventoryType.ENDER_CHEST);
+        temp.put(Material.TRAPPED_CHEST, InventoryType.CHEST);
+        temp.put(Material.ENCHANTMENT_TABLE, InventoryType.ENCHANTING);
+        temp.put(Material.DISPENSER, InventoryType.DISPENSER);
+        temp.put(Material.DROPPER, InventoryType.DROPPER);
+        temp.put(Material.FURNACE, InventoryType.FURNACE);
+        temp.put(Material.HOPPER, InventoryType.HOPPER);
+        temp.put(Material.WORKBENCH, InventoryType.WORKBENCH);
+        return Collections.unmodifiableMap(temp);
     }
     
     public static InventoryType convert(Material x) throws IllegalArgumentException {
@@ -33,9 +38,18 @@ public class BaseType {
         return emap.get(x);
     }
     
-    public static boolean contains(String x) {
+    public static boolean containsKey(String x) {
         Material k = Material.valueOf(x.toUpperCase());
         return emap.containsKey(k);
+    }
+    
+    public static boolean containsValue(String x) {
+        InventoryType v = InventoryType.valueOf(x.toUpperCase());
+        return emap.containsValue(v);
+    }
+    
+    public static Collection<InventoryType> values() {
+        return emap.values();
     }
     
 }
