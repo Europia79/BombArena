ba49d7f166b8cf6d0bb390f5d2a974925fadcf59
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-05-13 18:26:25 -0500

    Moved to utility package.
    Not sure if i changed anything else.
    
    But I also added comments that show when items were introduced into
    Minecraft.
    
    Which means that if you want to support Minecraft versions older than
    1.5.2 then you'll need to comment out or delete those items.
    
    Instead of hardcoding the items, I developed a loop from Material.values()
    but it was only able to reconize 8 out of 13 InventoryTypes. Hopefully, I
    can fix this in the future to support old AND new versions of Minecraft
    (i.e. blocks that haven't been released yet and have inventories
    associated with them).

--- src/mc/euro/demolition/util/BaseType.java
+++ src/mc/euro/demolition/util/BaseType.java
@@ -1,4 +1,4 @@
-package mc.euro.demolition.objects;
+package mc.euro.demolition.util;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -6,31 +6,33 @@ import java.util.EnumMap;
 import java.util.Map;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
 
 /**
- * Converts from Material.X to InventoryType.X
+ * Handles the conversion from Material.X to InventoryType.X
  */
 public class BaseType {
     
     private static final Map<Material, InventoryType> emap = initMap();
-    
+
     private static Map<Material, InventoryType> initMap() {
         Map<Material, InventoryType> temp = new EnumMap<Material, InventoryType>(Material.class);
-        temp.put(Material.ANVIL, InventoryType.ANVIL);
-        temp.put(Material.BEACON, InventoryType.BEACON);
+        temp.put(Material.ANVIL, InventoryType.ANVIL); // 1.4.2
+        temp.put(Material.BEACON, InventoryType.BEACON); // 1.4.2
         temp.put(Material.BREWING_STAND, InventoryType.BREWING);
         temp.put(Material.CHEST, InventoryType.CHEST);
-        temp.put(Material.ENDER_CHEST, InventoryType.ENDER_CHEST);
-        temp.put(Material.TRAPPED_CHEST, InventoryType.CHEST);
+        temp.put(Material.ENDER_CHEST, InventoryType.ENDER_CHEST); // 1.3.1
+        temp.put(Material.TRAPPED_CHEST, InventoryType.CHEST); // 1.5
         temp.put(Material.ENCHANTMENT_TABLE, InventoryType.ENCHANTING);
         temp.put(Material.DISPENSER, InventoryType.DISPENSER);
-        temp.put(Material.DROPPER, InventoryType.DROPPER);
+        temp.put(Material.DROPPER, InventoryType.DROPPER); // 1.5
         temp.put(Material.FURNACE, InventoryType.FURNACE);
-        temp.put(Material.HOPPER, InventoryType.HOPPER);
+        temp.put(Material.HOPPER, InventoryType.HOPPER); // 1.5
         temp.put(Material.WORKBENCH, InventoryType.WORKBENCH);
         return Collections.unmodifiableMap(temp);
+
     }
-    
+
     public static InventoryType convert(Material x) throws IllegalArgumentException {
         if (!emap.containsKey(x)) {
             throw new IllegalArgumentException(x.toString());
