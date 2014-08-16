e9334f14b9cefc4edeefa565a59b3dc8a6586ca6
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-08-16 00:47:53 -0500

    Refactored to make testing easier.
    https://www.youtube.com/watch?v=wEhu57pih5w&list=PLD0011D00849E1B79
    
    Misko says that contructors should only ask for what they need and make
    assignments to fields. Misko says constructors should not be doing "work"
    by asking for an object... And reaching thru that object to get what it
    really needs/wants.
    
    Originally, I wanted to make it easy for the end-user of this class to
    simply pass the name of the plugin, and have the Version class get that
    plugin (and its version).
    
    This "work" has been moved to the factory methods:
    getPluginVersion(String pluginName) : returns new Version object.
    getPluginVersion(Plugin plugin) : returns new Version object.
    getServerVersion() : returns new Version object.
    getNmsVersion() : returns new Version object.
    
    note on Constructors:
    Please use the factory methods for generating new Version objects.
    Because the old constructor
    new Version(String pluginName)
    is no longer valid. it has changed to
    new Version(String version)
    
    This new contructor is used by getSubVersion(regex).
    
    The field String version; is now final.
    VERSION OBJECTS ARE NOW IMMUTABLE.
    
    STRING WRAPPERS HAVE BEEN DELETED.
    
    Instead, just call toString() then call any String method you desire.
    
    Some example use cases:
    
    Version gm = Version.getPluginVersion("GroupManager");
    
    if (gm.toString().contains("Dev") { }
    
    Version enjin = Version.getPluginVersion("EnjinMinecraftPlugin");
    
    if (!enjin.isCompatibile("2.6.0") { disableThis(); }
    
    Version hd = Version.getPluginVersion("HolographicDisplays");
    
    if (!hd.isCompatibile("1.8.4") { }
    
    Version ba = Version.getPluginVersion("BattleArena");
    
    if (!ba.isCompatible("3.9.7.3") { disableThis(); }
    
    String NMS = Version.getNmsVersion().toString();
    
    Version server = Version.getServerVersion();
    
    if (!server.isSupported("1.7.10-9") || !server.isCompatible("1.2.5")) {
       disableThis();
    }

---
+++ src/main/java/mc/euro/demolition/util/Version.java
@@ -0,0 +1,179 @@
+package mc.euro.demolition.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * The Version object: Capable of asking the important question: <br/><br/>
+ * 
+ * Is the version that's currently installed & running on the server compatible with a specified version ? <br/><br/>
+ * 
+ * isCompatible(): Is the installed version greater than or equal to the minimum required version ? <br/><br/>
+ * 
+ * isSupported(): Is the installed version less than or equal to the maximum required version ? <br/><br/>
+ * 
+ * @author Europia79, BigTeddy98, Tux2
+ */
+public class Version implements Comparable<String> {
+    
+    Plugin plugin;
+    final String version;
+    String separator = "[.-]";
+
+    public Version(String version) {
+        this.version = version;
+    }
+    
+    public Version(Server server) {
+        this.plugin = null;
+        this.version = server.getBukkitVersion();
+    }
+    
+    public Version(Plugin plugin) {
+        this.plugin = (plugin == null) ? null : plugin;
+        this.version = (plugin == null) ? null : plugin.getDescription().getVersion();
+    }
+    
+    public static Version getPluginVersion(Plugin plugin) {
+        return new Version(plugin);
+    }
+    
+    public static Version getPluginVersion(String pluginName) {
+        return new Version(Bukkit.getPluginManager().getPlugin(pluginName));
+    }
+    
+    public static Version getServerVersion() {
+        return new Version(Bukkit.getServer());
+    }
+    
+    public static Version getNmsVersion() {
+        String NMS = null;
+            try {
+                NMS = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                NMS = "vpre";
+            }
+        return new Version(NMS);
+    }
+    
+    public Plugin getPlugin() {
+        return (plugin == null) ? null : plugin;
+    }
+    
+    public boolean isEnabled() {
+        if (this.plugin != null) {
+            return this.plugin.isEnabled();
+        } else if (this.version != null) { // No plugin, but object is a version checker for the server
+            return true;
+        }
+        return false; // Plugin mis-spelled or not installed on the server.
+    }
+    
+    /**
+     * @param minVersion - The absolute minimum version that's required to achieve compatibility.
+     * @return Greater than or equal to will return true. Otherwise, false.
+     */
+    public boolean isCompatible(String minVersion) {
+        if (!this.isEnabled()) return false;
+        int x = compareTo(minVersion);
+        if (x >= 0) {
+            return true;
+        } 
+        return false;
+    }
+    
+    /**
+     * @param maxVersion - The absolute maximum version that's supported.
+     * @return Less than or equal to will return true. Otherwise, false.
+     */
+    public boolean isSupported(String maxVersion) {
+        if (!this.isEnabled()) return false;
+        int x = compareTo(maxVersion);
+        if (x <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int compareTo(String whichVersion) {
+        int[] currentVersion = parseVersion(this.version);
+        int[] otherVersion = parseVersion(whichVersion);
+        int length = (currentVersion.length >= otherVersion.length) ? currentVersion.length : otherVersion.length;
+        for (int index = 0; index <= (length - 1); index = index + 1) {
+            try {
+                if (currentVersion[index] != otherVersion[index]) {
+                    if (currentVersion[index] > otherVersion[index]) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            } catch (IndexOutOfBoundsException ex) {
+                if (currentVersion.length > otherVersion.length) {
+                    return 1;
+                } else if (currentVersion.length < otherVersion.length) {
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+    
+    /**
+     * A typical version of 1.2.3.4-b567 will be broken down into an array. <br/><br/>
+     * 
+     * [1] [2] [3] [4] [567]
+     */
+    private int[] parseVersion(String version) {
+        String[] stringArray = version.split(separator);
+        int[] temp = new int[stringArray.length];
+        for (int index = 0; index <= (stringArray.length - 1); index = index + 1) {
+            String t = stringArray[index].replaceAll("\\D", "");
+            try {
+                temp[index] = Integer.valueOf(t);
+            } catch(NumberFormatException ex) {
+                temp[index] = 0;
+            }
+        }
+        return temp;
+    }
+    
+    public Version setSeparator(String regex) {
+        this.separator = regex;
+        return this;
+    }
+    
+    /**
+     * search() for possible Development builds.
+     */
+    public boolean search(String regex) {
+        if (version == null) return false;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(this.version);
+        if (matcher.find()) {
+            return true;
+        }
+        return false;
+    }
+    
+    public Version getSubVersion(String regex) {
+        if (version == null) return this;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(this.version);
+        String dev = this.version;
+        if (matcher.find()) {
+            dev = matcher.group();
+        }
+        return new Version(dev);
+    }
+    
+    @Override
+    public String toString() {
+        String v = (this.version == null) ? "" : this.version;
+        return v;
+    }
+}
