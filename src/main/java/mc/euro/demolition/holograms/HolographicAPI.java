8bf1bf80115139aa0e199dc11e5c15c2f05e5764
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-08-17 23:06:39 -0500

    HologramInterface implementations
    There are 3 implementations:
    HolographicDisplay.java
    HolographicAPI.java
    HologramsOff.java
    
    Tested and working. Only minor bugs.
    
    HoloAPI has a minor bug with VirtualPlayers.
    
    HolographicDisplays has a rare bug that occassionally displays holograms
    underground (for items thrown really high + far). But I think I can
    compensate for this behavior in the calling code.

--- src/main/java/mc/euro/demolition/holograms/HolographicAPI.java
+++ src/main/java/mc/euro/demolition/holograms/HolographicAPI.java
@@ -1,6 +1,10 @@
 package mc.euro.demolition.holograms;
 
-import mc.euro.demolition.BombPlugin;
+import com.dsh105.holoapi.HoloAPI;
+import com.dsh105.holoapi.api.Hologram;
+import com.dsh105.holoapi.api.HologramFactory;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
@@ -11,28 +15,60 @@ import org.bukkit.plugin.Plugin;
 public class HolographicAPI implements HologramInterface {
 
     Plugin plugin;
+    private final AtomicInteger ID = new AtomicInteger(0);
+    private final ConcurrentHashMap<Integer, Hologram> hmap = new ConcurrentHashMap<Integer, Hologram>();
     
     public HolographicAPI(Plugin plugin) {
         this.plugin = plugin;
     }
+    
     @Override
     public int createBaseHologram(Location loc) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Location center = loc.clone().add(0.5, 1.0, 0.5);
+        Hologram hologram = new HologramFactory(plugin)
+                .withLocation(center)
+                .withText("Target")
+                .withSimplicity(true)
+                .build();
+        int hologramID = addHologram(hologram);
+        return hologramID;
     }
 
     @Override
     public int createBombHologram(Location loc) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Location center = loc.clone().add(0.0, 0.2, 0.0);
+        Hologram hologram = new HologramFactory(plugin)
+                .withLocation(center)
+                .withText("Bomb")
+                .withSimplicity(true)
+                .build();
+        int hologramID = addHologram(hologram);
+        return hologramID;
     }
-
+    
     @Override
     public void teleport(int id, Location loc) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        getHologram(id).move(loc.clone().add(0.0, 0.4, 0.0));
     }
-
+    
     @Override
     public void removeHologram(int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        deleteHologram(id);
+    }
+    
+    protected int addHologram(Hologram hologram) {
+        int id = ID.incrementAndGet();
+        hmap.put(id, hologram);
+        return id;
+    }
+    
+    protected Hologram getHologram(int id) {
+        return hmap.get(id);
+    }
+    
+    protected void deleteHologram(int id) {
+        Hologram hologram = hmap.get(id);
+        HoloAPI.getManager().stopTracking(hologram);
     }
     
 }
--- src/main/java/mc/euro/demolition/holograms/HolographicDisplay.java
+++ src/main/java/mc/euro/demolition/holograms/HolographicDisplay.java
@@ -2,6 +2,8 @@ package mc.euro.demolition.holograms;
 
 import com.gmail.filoghost.holograms.api.Hologram;
 import com.gmail.filoghost.holograms.api.HolographicDisplaysAPI;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
@@ -9,12 +11,13 @@ import org.bukkit.plugin.Plugin;
  *
  * @author Nikolai
  */
-public class HolographicDisplay extends HoloManager implements HologramInterface {
+public class HolographicDisplay implements HologramInterface {
     
     Plugin plugin;
+    private final AtomicInteger ID = new AtomicInteger(0);
+    private final ConcurrentHashMap<Integer, Hologram> hmap = new ConcurrentHashMap<Integer, Hologram>();
     
     public HolographicDisplay(Plugin plugin) {
-        super();
         this.plugin = plugin;
     }
     
@@ -42,6 +45,21 @@ public class HolographicDisplay extends HoloManager implements HologramInterface
     @Override
     public void removeHologram(int id) {
         deleteHologram(id);
+    }
+    
+    protected int addHologram(Hologram hologram) {
+        int id = ID.incrementAndGet();
+        hmap.put(id, hologram);
+        return id;
+    }
+    
+    protected Hologram getHologram(int id) {
+        return hmap.get(id);
+    }
+    
+    protected void deleteHologram(int id) {
+        Hologram hologram = hmap.get(id);
+        hologram.delete();
     }
     
 }
